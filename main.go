29e4ac14dc11256e08a108ce70d2b95368de8b0c
Theodor René Carlsen <thca@dr.dk>
2025-04-27 19:08:15 +0200

    changes

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
-	"github.com/yuin/goldmark"
 	"html/template"
 	"os"
 	"regexp"
+	"strings"
+
+	"github.com/yuin/goldmark"
 )
 
 func check(e error) {
@@ -19,13 +22,23 @@ func check(e error) {
 
 func main() {
 	recreateBuildFolder()
+	addStyleSheets()
+	fmt.Println("Stylesheet(s) added")
 	buildFrontPage()
+	fmt.Println("Frontpage added")
 	buildBlogPosts()
+	fmt.Println("Build done")
 }
 func recreateBuildFolder() {
 	err := os.RemoveAll("build")
 	check(err)
 	os.Mkdir("build", 0755)
+}
+func addStyleSheets() {
+	dat, err := os.ReadFile("templates/styles.css")
+	check(err)
+	err = os.WriteFile("build/styles.css", dat, 0755)
+	check(err)
 }
 
 func buildFrontPage() {
@@ -35,13 +48,60 @@ func buildFrontPage() {
 	postNames := getPostNames()
 	t.Execute(&buf, template.HTML(generateLinkList(postNames)))
 	os.WriteFile("build/index.html", buf.Bytes(), 0755)
-	fmt.Println("build done")
+}
+
+func buildBlogPosts() {
+	dir, err := os.ReadDir("posts")
+	tmplate, err := getTemplate("post", "templates/post.template.html")
+	check(err)
+	for _, blogPost := range dir {
+
+		nameWithoutExt := blogPost.Name()[:len(blogPost.Name())-3]
+		data, err := os.ReadFile("posts/" + blogPost.Name())
+		check(err)
+		htmlString := mdToHtml(data).String()
+		var buf bytes.Buffer
+		tmplate.Execute(&buf, template.HTML(htmlString))
+		os.WriteFile("build/"+nameWithoutExt+".html", buf.Bytes(), 0755)
+	}
+	fmt.Println("Posts added")
+}
+
+type PostMetadata struct {
+	title       string
+	tags        []string
+	draft       bool
+	description string
+	date        string // for now
+}
+
+func parseMetadata(post []byte) *PostMetadata {
+	scanner := bufio.NewScanner(strings.NewReader(string(post)))
+	var lines []string
+	var metaData PostMetadata
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "+++" {
+			for scanner.Scan() {
+				if line == "+++" {
+					break
+				}
+				lines = append(lines, line)
+				// use regex to get each side
+			}
+			break
+		}
+	}
+
+
 }
 
 func generateLinkList(postNames []string) (res string) {
 	res += "<ul>\n"
 	for _, name := range postNames {
-		res += fmt.Sprintf("<li><a href='/%s.html'> %s </a></li>\n", name, name)
+		name = name[:len(name)-3] // Remove the .md extension
+		escapedName := template.HTMLEscapeString(name)
+		res += fmt.Sprintf("<li><a href='/%s.html'> %s </a></li>\n", escapedName, escapedName)
 	}
 	res += "</ul>"
 	return
@@ -53,20 +113,6 @@ func getPostNames() (postNames []string) {
 		postNames = append(postNames, post.Name())
 	}
 	return
-}
-
-func buildBlogPosts() {
-	dir, err := os.ReadDir("posts")
-	tmplate, err := getTemplate("post", "templates/post.template.html")
-	check(err)
-	for _, blogPost := range dir {
-		data, err := os.ReadFile("posts/" + blogPost.Name())
-		check(err)
-		htmlString := mdToHtml(data).String()
-		var buf bytes.Buffer
-		tmplate.Execute(&buf, template.HTML(htmlString))
-		os.WriteFile("build/"+blogPost.Name()+".html", buf.Bytes(), 0755)
-	}
 }
 
 func mdToHtml(markdownBuffer []byte) *bytes.Buffer {
